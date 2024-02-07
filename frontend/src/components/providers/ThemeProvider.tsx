240cd8ed63ea10f24e9dcb7dc9a618a45227cb31
Dagem Tadesse <43210896+dagemtadesse@users.noreply.github.com>
2024-02-07 21:15:59 +0300

    setup: icons and theme

--- frontend/src/components/providers/ThemeProvider.tsx
+++ frontend/src/components/providers/ThemeProvider.tsx
@@ -1,10 +1,26 @@
-"use client"
+"use client";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { ReactNode } from "react";
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2563eb",
+    },
+    secondary: {
+      main: "#ffffff",
+    },
+    text: {
+      primary: "#fff",
+    },
+    background: {
+      default: "#110f0f",
+      paper: "#121212",
+    },
+  },
+});
 
-export const CustomThemeProvider = ({children}: {children: ReactNode}) => {
+export const CustomThemeProvider = ({ children }: { children: ReactNode }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
