693371f6cbc586e51e74f9f26e464d2d66da57f3
yeabsra-leul <74534830+yeabsra-leul@users.noreply.github.com>
2024-02-08 23:14:38 +0300

    feat : add authguard

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -22,9 +22,12 @@ import PersonIcon from "@mui/icons-material/Person";
 import { Logo } from "./Logo";
 import { useState } from "react";
 import Link from "next/link";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Header = () => {
   const theme = useTheme();
+  const { isAuthenticated, logout } =
+    useAuth0();
 
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -52,7 +55,6 @@ export const Header = () => {
             <Typography variant="h5">Pulsar</Typography>
           </Stack>
         </Button>
-
         <Stack
           direction="row"
           sx={{ ml: "auto" }}
@@ -62,69 +64,84 @@ export const Header = () => {
           <Button LinkComponent={Link} href="/docs" color="secondary">
             Docs
           </Button>
-          <Button
-            LinkComponent={Link}
-            href={`/${username}/projects`}
-            color="secondary"
-          >
-            Projects
+          <Button LinkComponent={Link} href="/docs" color="secondary">
+            Pricing
           </Button>
-          <Button variant="contained" startIcon={<AddRoundedIcon />}>
-            Create Project
-          </Button>
-          <IconButton
-            id="profile-button"
-            aria-controls={open ? "profile-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-          >
-            <Avatar sx={{ width: 24, height: 24 }} />
-          </IconButton>
+          {isAuthenticated && (
+            <>
+              <Button
+                LinkComponent={Link}
+                href={`/${username}/projects`}
+                color="secondary"
+              >
+                Projects
+              </Button>
+              <Button variant="contained" startIcon={<AddRoundedIcon />}>
+                Create Project
+              </Button>
+              <IconButton
+                id="profile-button"
+                aria-controls={open ? "profile-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <Avatar sx={{ width: 24, height: 24 }} />
+              </IconButton>
 
-          <Menu
-            id="profile-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "profile-button",
-            }}
-            sx={{
-              "& .MuiList-root": { p: 0 },
-              "& .MuiPaper-root": { minWidth: 240, maxWidth: 290 },
-            }}
-          >
-            <Stack
-              direction={"row"}
-              gap={5}
-              alignItems={"center"}
-              sx={{ width: "100%", justifyContent: "space-between", p: 2 }}
-            >
-              <Box>
-                <Typography
-                  sx={{ whiteSpace: "nowrap" }}
-                  variant="subtitle1"
-                  gutterBottom
+              <Menu
+                id="profile-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "profile-button",
+                }}
+                sx={{
+                  "& .MuiList-root": { p: 0 },
+                  "& .MuiPaper-root": { minWidth: 240, maxWidth: 290 },
+                }}
+              >
+                <Stack
+                  direction={"row"}
+                  gap={5}
+                  alignItems={"center"}
+                  sx={{ width: "100%", justifyContent: "space-between", p: 2 }}
                 >
-                  Dagem Tadesse
-                </Typography>
-                <Typography variant="body2">Developer</Typography>
-              </Box>
-              <Tooltip title="Logout">
-                <IconButton>
-                  <LogoutRoundedIcon />
-                </IconButton>
-              </Tooltip>
-            </Stack>
-            <Divider />
-            <MenuItem sx={{ py: 2 }}>
-              <ListItemIcon>
-                <PersonIcon />
-              </ListItemIcon>
-              <ListItemText primary={"View Profile"} />
-            </MenuItem>
-          </Menu>
+                  <Box>
+                    <Typography
+                      sx={{ whiteSpace: "nowrap" }}
+                      variant="subtitle1"
+                      gutterBottom
+                    >
+                      Dagem Tadesse
+                    </Typography>
+                    <Typography variant="body2">Developer</Typography>
+                  </Box>
+                  <Tooltip title="Logout">
+                    <IconButton
+                      onClick={() =>
+                        logout({
+                          logoutParams: {
+                            returnTo: process.env.NEXT_PUBLIC_redirectUri,
+                          },
+                        })
+                      }
+                    >
+                      <LogoutRoundedIcon />
+                    </IconButton>
+                  </Tooltip>
+                </Stack>
+                <Divider />
+                <MenuItem sx={{ py: 2 }}>
+                  <ListItemIcon>
+                    <PersonIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={"View Profile"} />
+                </MenuItem>
+              </Menu>
+            </>
+          )}
         </Stack>
       </Toolbar>
     </AppBar>
