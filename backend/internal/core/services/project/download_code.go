90a90a16e14d9329488d7f682951d7cc83cbe5cf
Dagem Tadesse <43210896+dagemtadesse@users.noreply.github.com>
2024-02-21 22:09:58 +0300

    fix(backend): remove fmt.Print

--- backend/internal/core/services/project/download_code.go
+++ backend/internal/core/services/project/download_code.go
@@ -2,7 +2,6 @@ package project
 
 import (
 	"context"
-	"fmt"
 	"pulsar/internal/core/services"
 )
 
@@ -17,10 +16,7 @@ func (projectService *ProjectService) DownloadProjectCode(ctx context.Context, r
 		return "", services.NewAppError(services.ErrNotFound, err)
 	}
 
-	fmt.Print(project.SourceCode.URI)
 	zippedCode, err := projectService.fileRepo.ZipSourceCode(project.SourceCode.URI)
-	fmt.Println(zippedCode.Name())
-
 	if err != nil {
 		return "", services.NewAppError(services.ErrInternalServer, err)
 	}
