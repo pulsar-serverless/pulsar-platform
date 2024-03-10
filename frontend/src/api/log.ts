c356e942f683623cc1ae67f6dc0ede84ed8841a6
Dagem Tadesse <43210896+dagemtadesse@users.noreply.github.com>
2024-03-11 00:37:31 +0300

    feat (frontend): filter logs

--- frontend/src/api/log.ts
+++ frontend/src/api/log.ts
@@ -5,13 +5,15 @@ import { Paginated } from "@/models/pagination";
 export const LogApi = {
   async getLogs(
     projectID: string,
+    logTypes: Log['type'][],
+    searchQuery: string,
     pageNumber: number = 1,
     pagesize: number = 50
   ) {
     const { data } = await axiosInstance.get<Paginated<Log>>(
       `/projects/logs/${projectID}`,
       {
-        params: { pageNumber, pagesize },
+        params: { pageNumber, pagesize, logTypes, searchQuery },
       }
     );
     return data;
