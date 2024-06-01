a4515289423b957c885758ebced43318e670ef35
Naomi Timothy <naomitim17.com>
2024-06-01 15:58:40 +0300

    material ui update

--- frontend/src/components/resources/ResourceCards.tsx
+++ frontend/src/components/resources/ResourceCards.tsx
@@ -1,6 +1,6 @@
 // ResourceCards.tsx
 import React from "react";
-import "./resource.css";
+import { Card, CardContent, Typography } from "@mui/material";
 
 interface TotalResourceUtil {
 	mem_usage_mb: number;
@@ -19,32 +19,52 @@ const ResourceCards: React.FC<ResourceCardsProps> = ({ totalResourceUtil }) => {
 			{totalResourceUtil && (
 				<div className="parent-card">
 					{/* Card for mem_usage_mb */}
-					<div className="card">
-						<div className="card-body">
-							<h5 className="card-title">Memory Usage</h5>
-							<p className="card-text">
-								{totalResourceUtil.mem_usage_mb} Megabytes
-							</p>
-						</div>
-					</div>
+					<Card className="card">
+						<CardContent className="card-body">
+							<Typography variant="h5" component="div" className="card-title">
+								Memory Usage
+							</Typography>
+							<Typography
+								variant="body2"
+								color="text.secondary"
+								className="card-text">
+								hi
+								{/* {totalResourceUtil.mem_usage_mb} Megabytes */}
+							</Typography>
+						</CardContent>
+					</Card>
 
 					{/* Card for net_usage_mb */}
-					<div className="card">
-						<div className="card-body">
-							<h5 className="card-title">Network Usage</h5>
-							<p className="card-text">
-								{totalResourceUtil.net_usage_mb} Megabytes
-							</p>
-						</div>
-					</div>
+					<Card className="card">
+						<CardContent className="card-body">
+							<Typography variant="h5" component="div" className="card-title">
+								Network Usage
+							</Typography>
+							<Typography
+								variant="body2"
+								color="text.secondary"
+								className="card-text">
+								hi
+								{/* {totalResourceUtil.net_usage_mb} Megabytes */}
+							</Typography>
+						</CardContent>
+					</Card>
 
 					{/* Card for usage_period */}
-					<div className="card">
-						<div className="card-body">
-							<h5 className="card-title">Usage Period</h5>
-							<p className="card-text">{totalResourceUtil.usage_period}</p>
-						</div>
-					</div>
+					<Card className="card">
+						<CardContent className="card-body">
+							<Typography variant="h5" component="div" className="card-title">
+								Usage Period
+							</Typography>
+							<Typography
+								variant="body2"
+								color="text.secondary"
+								className="card-text">
+								hello
+								{/* {totalResourceUtil.usage_period} */}
+							</Typography>
+						</CardContent>
+					</Card>
 				</div>
 			)}
 		</div>
